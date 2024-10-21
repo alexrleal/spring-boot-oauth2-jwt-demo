55cfb649a25ef2cde19ee4105fc2e7564d4cfca9
Alexandre Leal <alexrleal0@gmail.com>
2024-10-21 14:12:46 -0300

    Authorization server, Resource server, not tested

--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$aunL5GFPsfpMNx7dmUlWM.27RegaeEJ/zSdjufJa.6eSdbsGMSoGm');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$aunL5GFPsfpMNx7dmUlWM.27RegaeEJ/zSdjufJa.6eSdbsGMSoGm');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
